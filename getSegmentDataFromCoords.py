ec43a2c4685c341d98f1f1cbafafb8e63f353703
Danny jaramillo <44910053+djara1214@users.noreply.github.com>
2023-09-13 16:07:50 -0400

    Finished/Corrected API calls to save Segment data
    rename getDataWorks.py to getSegmentDataFromCoords.py
    Cleanup
    Finished Implementation- Next Step: Combine csv's from different runs and combine unique segment numbers to then use for web scrapping

--- getSegmentDataFromCoords.py
+++ getSegmentDataFromCoords.py
@@ -1,9 +1,9 @@
 import requests
 import time
 import os
-from dotenv import load_dotenv
 import array as ar
 import pandas as pd
+from dotenv import load_dotenv
 
 load_dotenv()
 #-------------Functions-------------
@@ -35,20 +35,11 @@ def divide_area(southwest_lat, southwest_lon, northeast_lat, northeast_lon, n):
     return divided_areas
 
 #-------------Main-------------
-#Df creation, changed from txt file     8/24
 
 #DF Creation, create temp and hold data in df_segments
 columns =['SegName','SegNum']
 df=pd.DataFrame(columns=columns)
 df_Segments=df.astype({"SegNum":'int64'},copy=False)
-
-#bounds =  # array[Float] | [southwest corner latitutde, southwest corner longitude, northeast corner latitude, northeast corner longitude]
-tempListLocations="40.60034008410533, -73.9948294872619,40.65157646433741, -73.92683111672085"
-#tempListLocations='40.5411, -74.04795, 40.5437511, -74.0450481'
-# ne=(40.80621,-73.75776)
-# sw=(40.54110,-74.04795)
-
-
 
 #Timer vars to work under API limits
 maxRequests=100
@@ -61,13 +52,8 @@ startTime=time.time()
 api_url = 'https://www.strava.com/api/v3/segments/explore'
 
 #n= number of split areas created in larger map
-n=5
+n=100
 coordList=divide_area(40.54110,-74.04795,40.80621,-73.75776, n)
-# coordList=divide_area(40.70419691822427, -73.81544109047971,40.73312306812104, -73.78282145859072,n)
-
-# for i in range(5):
-#     print(cordListFinal[i])
-# Set other parameters for the explore request (optional)
 radius = 1000  # in meters
 min_cat = 0   # minimum climb category (0-5)
 max_cat = 56   # maximum climb category (0-5)
@@ -83,11 +69,10 @@ for coord in coordList:
         time.sleep(timerInterval - (currentTime-startTime))
         startTime=time.time()
         request_count=0
-
+        
     headers = {'Authorization': f'Bearer {os.getenv("access_token")}'}
     params = {
-        #'bounds':tempListLocations,    SMALL TEST LOCATION (Brooklyn)
-        'bounds':coordList,
+        'bounds':coord,
         'activity_type': 'riding',
         }
 
@@ -101,12 +86,12 @@ for coord in coordList:
             segment_id = segment['id']
             df_Temp=pd.DataFrame({'SegName': [segment_name],'SegNum':[segment_id]})
             df_Segments=pd.concat([df_Segments,df_Temp],ignore_index=True,axis=0)
-            print(f'Segment Name: {segment_name}')
-            print(f'Segment ID: {segment_id}')
+            # print(f'Segment Name: {segment_name}')
+            # print(f'Segment ID: {segment_id}')
     else:
         print(f'Error: {response.status_code} - {response.text}')
 
     requestCount+=1
     time.sleep(1)
 
-df_Segments.to_csv('saved_seg_data2.csv.csv', index=False)
+    df_Segments.to_csv('segmentData.csv', index=False)
